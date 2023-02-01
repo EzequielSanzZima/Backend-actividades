24cfc1e2c037f6b3c55e66ae3a7d4baeb1704b59
Ezequiel Sanz Zima <ezequielsanzzima@hotmail.com>
2023-01-31 22:52:11 -0300

    Inicio de sesión

---
+++ Tp11 - Inicio de sesión/src/main.js
@@ -0,0 +1,78 @@
+import express from 'express'
+import session from 'express-session'
+import MongoStore from 'connect-mongo'
+
+import config from './config.js'
+
+import { Server as HttpServer } from 'http'
+import { Server as Socket } from 'socket.io'
+
+import authWebRouter from './routers/web/auth.js'
+import homeWebRouter from './routers/web/home.js'
+import productosApiRouter from './routers/api/productos.js'
+
+import addProductosHandlers from './routers/ws/productos.js'
+import addMensajesHandlers from './routers/ws/mensajes.js'
+
+import objectUtils from './utils/objectUtils.js'
+
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+//--------------------------------------------
+// instancio servidor, socket y api
+
+const app = express()
+const httpServer = new HttpServer(app)
+const io = new Socket(httpServer)
+app.use(passport.initialize())
+
+//--------------------------------------------
+// configuro el socket
+
+io.on('connection', async socket => {
+    console.log('Nuevo cliente conectado!');
+    addProductosHandlers(socket, io.sockets)
+    addMensajesHandlers(socket, io.sockets)
+});
+
+//--------------------------------------------
+// configuro el servidor
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+
+app.set('view engine', 'ejs');
+
+app.use(cookieParser())
+app.use(objectUtils.createOnMongoStore())
+
+// MIDDLEWARE PASSPORT
+app.use(passport.initialize())
+app.use(passport.session())
+
+import auth from './routers/web/auth.js'
+const sessions = auth
+app.use('/api/sessions', sessions)
+
+
+//--------------------------------------------
+// rutas del servidor API REST
+
+app.use(productosApiRouter)
+
+//--------------------------------------------
+// rutas del servidor web
+
+app.use(authWebRouter)
+app.use(homeWebRouter)
+
+//--------------------------------------------
+// inicio el servidor
+
+const connectedServer = httpServer.listen(config.PORT, () => {
+    console.log(`Servidor http escuchando en el puerto ${connectedServer.address().port}`)
+})
+connectedServer.on('error', error => console.log(`Error en servidor ${error}`))
