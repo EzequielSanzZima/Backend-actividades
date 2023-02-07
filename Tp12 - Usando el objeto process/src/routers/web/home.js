c77a3968764059530f30b48d7a1c25a67d4ddb44
Ezequiel Sanz Zima <ezequielsanzzima@hotmail.com>
2023-02-07 16:50:44 -0300

    Usando el objeto process

---
+++ Tp12 - Usando el objeto process/src/routers/web/home.js
@@ -0,0 +1,40 @@
+import { Router } from 'express'
+import { webAuth } from '../../auth/index.js'
+import { fork } from 'child_process'
+
+import path from 'path'
+
+const productosWebRouter = new Router()
+
+productosWebRouter.get('/home', webAuth, (req, res) => {
+    res.render(path.join(process.cwd(), '/views/pages/home.ejs'), { nombre: req.session.nombre })
+})
+
+productosWebRouter.get('/productos-vista-test', (req, res) => {
+    res.sendFile(path.join(process.cwd(), '/views/productos-vista-test.html'))
+})
+
+productosWebRouter.get('/info', (req, res) => {
+    res.render(path.join(process.cwd(), 'views/info.ejs'), {
+        argsEntrada: process.argv.slice(2), 
+        sistOperativo: process.platform, 
+        node: process.version, 
+        rss: process.memoryUsage.rss(), 
+        pathEjecucion: process.execPath, 
+        pid: process.pid, 
+        carpetaProyecto: process.cwd()})
+})
+
+productosWebRouter.get('/api/randoms', (req,res)=>{
+    const child = fork('./src/randomNumber/randomNum.js')
+    const cantNum =  req.query.cantNum || 100000000
+    child.on('message', (childMsg)=>{
+        if (childMsg == 'Listo') {
+            child.send('Iniciar ' + cantNum)
+        } else {
+            res.render('randomNum', {childMsg: JSON.stringify(childMsg)})
+        }
+    })
+})
+
+export default productosWebRouter
